7963f5aba457e1bf2bd633bb1c07bc8af7cd2534
peppepika <gquadrodprimo@outlook.it>
2022-12-22 10:12:30 +0000

    feat: added number generator and username check

--- number_guess.sh
+++ number_guess.sh
@@ -7,3 +7,14 @@ echo -e "\n~~ Number Guessing Game ~~\n"
 NUMBER=$(( RANDOM % 1000 + 1 ))
 echo Enter you username:
 read NAME
+
+#check if username is already present
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$NAME'")
+if [[ -z $USER_ID ]] #true for new user
+then
+  $PSQL "INSERT username VALUES('$NAME')"
+  echo "Welcome, $NAME! It looks like this is your first time here."
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$NAME'")
+else
+  echo "Welcome back, $NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
