e957fd91b0fadbfd18c5fe5e8ab8397341ae3f93
peppepika <gquadrodprimo@outlook.it>
2022-12-22 10:00:07 +0000

    feat: added random number generator

--- number_guess.sh
+++ number_guess.sh
@@ -4,5 +4,6 @@ PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -
 
 echo -e "\n~~ Number Guessing Game ~~\n"
 
+NUMBER=$(( RANDOM % 1000 + 1 ))
 echo Enter you username:
 read NAME
