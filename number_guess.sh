ebea75ad8c7bf3706720ac647626c6004a9b7c6a
peppepika <gquadrodprimo@outlook.it>
2022-12-24 15:27:12 +0000

    fix: incorrect prompt

--- number_guess.sh
+++ number_guess.sh
@@ -5,7 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo -e "\n~~ Number Guessing Game ~~\n"
 
 NUMBER=$(( RANDOM % 1000 + 1 ))
-echo Enter you username:
+echo -e 'Enter your username:'
 read NAME
 
 #check if username is already present
